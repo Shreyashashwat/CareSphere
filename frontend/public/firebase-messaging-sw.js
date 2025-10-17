0f4724510a280ce8fb2cad8939e0faeaefc8a45d
Ishita Singhe <ishitasingh8170@gmail.com>
2025-10-17 22:44:13 +0530

    firebase modified

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,24 +1,31 @@
-// C:\Users\HP\Desktop\coding\CareSphere\frontend\public\firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+import { clientsClaim, skipWaiting } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
 
-firebase.initializeApp({
+skipWaiting();
+clientsClaim();
+precacheAndRoute(self.__WB_MANIFEST);
+
+const firebaseConfig = {
   apiKey: "AIzaSyAEWnPNtu9gQt7C7FkkPRKGdIVgPm7adas",
   authDomain: "caresphere-474703.firebaseapp.com",
   projectId: "caresphere-474703",
   storageBucket: "caresphere-474703.firebasestorage.app",
   messagingSenderId: "748085462199",
   appId: "1:748085462199:web:9a5ad7823e59000c2bf932"
-});
+};
 
-const messaging = firebase.messaging();
+initializeApp(firebaseConfig);
+const messaging = getMessaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log("Received background message ", payload);
+// handle background messages
+onBackgroundMessage(messaging, (payload) => {
+  console.log("Received background message", payload);
 
   const { title, body } = payload.notification;
   self.registration.showNotification(title, {
     body,
-    icon: "/logo192.png",
+    icon: "/Asset 2.png",
   });
 });
